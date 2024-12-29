f7e64a7df453536adfeff58de7b097c733f9f45e
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-12-29 14:16:54 +0600

    added banner 2 with bg

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,6 +6,18 @@ const Home = () => {
     <div>
       <Banner></Banner>
       <Category></Category>
+
+      {/* some random banner */}
+      <div className="hero min-h-[calc(100vh-200px)] bg-homeBg bg-no-repeat bg-cover bg-center flex items-center justify-center mb-16 md:mb-28">
+        <div className="w-11/12 md:w-8/12 bg-white text-center py-11 md:py-24 px-8 md:px-14">
+          <h1 className="text-2xl md:text-4xl mb-3">Bistro Boss</h1>
+          <p>
+            Monotonectally evisculate team driven resources vis-a-vis
+            next-generation markets. Proactively disintermediate user-centric
+            core competencies via cost effective growth strategies.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
